f813a9408524435f94479a770fe5b901e7e9d2fd
VenomSolo <karolikjakub@gmail.com>
2018-03-10 20:07:12 +0100

    Jakieś sterowanie + logi z builda

--- Source/Nexon/BaseCharacter.cpp
+++ Source/Nexon/BaseCharacter.cpp
@@ -1,6 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BaseCharacter.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/InputComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Camera/CameraComponent.h"
 
 
 // Sets default values
@@ -9,20 +14,37 @@ ABaseCharacter::ABaseCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->TargetArmLength = 1000.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+												// Create a camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	Camera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	Camera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	Camera->SetIsReplicated(false);
 }
 
 // Called when the game starts or when spawned
 void ABaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
 void ABaseCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	Rotation = UKismetMathLibrary::FindLookAtRotation(this->GetTargetLocation(), this->GetTargetLocation() + FVector(RotationX, RotationY, 0));
+	this->SetActorRotation(Rotation);
 }
 
 // Called to bind functionality to input
@@ -33,6 +55,10 @@ void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &ABaseCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ABaseCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("RotateRight", this, &ABaseCharacter::SetRotationY);
+	PlayerInputComponent->BindAxis("RotateForward", this, &ABaseCharacter::SetRotationX);
+	PlayerInputComponent->BindAction("Fire", this, &ABaseCharacter::Fire);
+	PlayerInputComponent->BindAction("FireAlternative", this, &ABaseCharacter::FireAlternative);
 
 }
 
@@ -50,5 +76,29 @@ void ABaseCharacter::MoveRight(float Value)
 	{
 		AddMovementInput(RIGHTVECTOR, Value);
 	}
+}
+
+void ABaseCharacter::SetRotationX(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		RotationX = Value;
+	}
+}
+
+void ABaseCharacter::SetRotationY(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		RotationY = Value;
+	}
+}
+
+void ABaseCharacter::Fire()
+{
+}
+
+void ABaseCharacter::FireAlternative()
+{
 }
 
--- Source/Nexon/BaseCharacter.h
+++ Source/Nexon/BaseCharacter.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
 #include "BaseCharacter.generated.h"
 
 UCLASS()
@@ -14,7 +17,16 @@ class NEXON_API ABaseCharacter : public ACharacter
 private:
 	const FVector FORWARDVECTOR = FVector(1.0, 0.0, 0.0);
 	const FVector RIGHTVECTOR = FVector(0.0, 1.0, 0.0);
+	float RotationX;
+	float RotationY;
+	FRotator Rotation;
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class USpringArmComponent* CameraBoom;
 
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* Camera;
 public:
 	// Sets default values for this character's properties
 	ABaseCharacter();
@@ -37,5 +49,21 @@ protected:
 
 	// Called for side to side input 
 	void MoveRight(float Value);
+
+	void SetRotationX(float Value);
+
+	void SetRotationY(float Value);
+
+	void Fire();
+
+	void FireAlternative();
 	
 };
+
+/*"Plugins": [
+{
+"Name": "ActorPool",
+"Enabled": true,
+"MarketplaceURL": "com.epicgames.launcher://ue/marketplace/content/66efc9b030dc4fbe9523d72b06f43a79"
+}
+]*/
